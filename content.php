76986b9d025deb3d184be1e0ecb6bd44c6d51b51
Henry Bayuzick <henrybayuzick@gmail.com>
2014-11-06 09:47:25 -0600

    Updated category names

--- content.php
+++ content.php
@@ -1,11 +1,11 @@
 <?php 
-if (in_category('link')) { 
+if (in_category('links')) { 
 	get_template_part('link');
-} else if (in_category('photo')) {
+} else if (in_category('photos')) {
 	get_template_part('photo' );
-} else if (in_category('video')) {
+} else if (in_category('videos')) {
 	get_template_part('video' );
-} else if (in_category('article')) {
+} else if (in_category('articles')) {
 	get_template_part('article', 'feed' );
 }
 ?>
